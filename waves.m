41912d4d58447532b64a21d4c72e5c76db51f3c5
amelim-laptop <Andrew.Melim@gatech.edu>
2012-04-20 16:17:47 -0400

    Air waves

---
+++ waves.m
@@ -0,0 +1,16 @@
+function vf = waves(vf)
+
+%Determine bottom left corner of vector
+w_w = 10;
+w_h = 5;
+w_xy = randi([-40 40],2,vf.nt);
+
+for x = 1:vf.detail:vf.size
+	for y = 1:vf.detail:vf.size
+		for z = 1:vf.detail:vf.size
+
+        end
+    end
+end
+
+end
